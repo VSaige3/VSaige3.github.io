1bf942984e76e660bb498c46b6e870cc68983cdc
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2020-07-22 11:08:12 -0500

    Update wip.php

--- wip.php
+++ wip.php
@@ -1,8 +1,12 @@
-<?php
-echo "<h2 align='center'>";
-if(!empty($_GET()['name'])) { echo "The project \"" . $_GET()['name'] . "\" does not exist yet!"; }
-else { echo "The project you are looking for does not exist yet"; }
-echo "</h2><br/>Come again later!<br/>";
-echo "<br/><br/>";
-if(!empty($_GET()['from'])) { echo "<a href=\"" . $_GET['from'] . "\">Go back</a>"; }
-?>
+<html>
+  <body>
+    <?php
+      echo "<h2 align='center'>";
+      if(!empty($_GET()['name'])) { echo "The project \"" . $_GET()['name'] . "\" does not exist yet!"; }
+      else { echo "The project you are looking for does not exist yet"; }
+      echo "</h2><br/>Come again later!<br/>";
+      echo "<br/><br/>";
+      if(!empty($_GET()['from'])) { echo "<a href=\"" . $_GET['from'] . "\">Go back</a>"; }
+    ?>
+  </body>
+</html>
