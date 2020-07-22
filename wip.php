2811efef57ff2c5345636a32c3624dc4ad5d4ff2
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2020-07-22 11:00:37 -0500

    Create wip.php

---
+++ wip.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2 align='center'>";
+if(!empty($_GET()['name'])) { echo "The project \"" . $_GET()['name'] . "\" does not exist yet!"; }
+else { echo "The project you are looking for does not exist yet"; }
+echo "</h2><br/>Come again later!<br/>";
+echo "<br/><br/>";
+if(!empty($_GET()['from'])) { echo "<a href=\"" . $_GET['from'] . "\">Go back</a>"; }
+?>
