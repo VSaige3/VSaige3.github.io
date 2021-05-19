d11fef9b360b60f84b42f7d2fc14c40de9e92ddc
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2021-05-19 12:00:12 -0500

    Create breakgradebook.js

---
+++ pages/api/breakgradebook.js
@@ -0,0 +1,7 @@
+function VS_C(){
+  var a=toString*$("vs__1").val());
+  var b=$("vs__2").val();
+  var c=toString($("vs__3").val());
+  $("vs__4").html(a+b+c+"="+toString(exec(a+b+c));
+}
+$("vs__button").click(VC_C);
