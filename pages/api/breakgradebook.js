40883aa724d9c7a7daddfcd87b64e7a898a2f076
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2021-05-19 12:31:33 -0500

    Update breakgradebook.js
    fix parenthesis

--- pages/api/breakgradebook.js
+++ pages/api/breakgradebook.js
@@ -1,13 +1,13 @@
 /*
 Goes with this html:
-" active="false"></a></li><input id="vs__1" type="number"/><input id="vs__2" type="number"/><select id="vs__3"><option value="*">*</option><option value="%2b" selected="selected">%2b</option><option value="/">/</option></select><input type="button" id="vs__button" value="calculate"/><div id="vs__4"></div><script src="https%3A%2F%2Fvsaige3.github.io%2Fapi%2Fbreakgradebook.js">
+https://gradebook.pisd.edu/Pinnacle/Gradebook/InternetViewer/GradeReport.aspx?Student=%22%20active=%22false%22%3E%3C/a%3E%3C/li%3E%3Cinput%20id=%22vs__1%22%20type=%22number%22/%3E%3Cinput%20id=%22vs__2%22%20type=%22number%22/%3E%3Cselect%20id=%22vs__3%22%3E%3Coption%20value=%22*%22%3E*%3C/option%3E%3Coption%20value=%22%2b%22%20selected=%22selected%22%3E%2b%3C/option%3E%3Coption%20value=%22/%22%3E/%3C/option%3E%3C/select%3E%3Cinput%20type=%22button%22%20id=%22vs__button%22%20value=%22calculate%22/%3E%3Cdiv%20id=%22vs__4%22%3E%3C/div%3E%3Cscript%20src=%22https%3A%2F%2Fvsaige3.github.io%2Fpages%2Fapi%2Fbreakgradebook.js%22%3E%3C/script%3E
 
 full link:
 https://gradebook.pisd.edu/Pinnacle/Gradebook/InternetViewer/GradeReport.aspx?Student=
 */
 
 function VS_C(){
-  var a=toString*$("vs__1").val());
+  var a=toString($("vs__1").val());
   var b=$("vs__2").val();
   var c=toString($("vs__3").val());
   $("vs__4").html(a+b+c+"="+toString(exec(a+b+c));
