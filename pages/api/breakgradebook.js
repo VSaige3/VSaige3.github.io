ef23cf353efbe96eaa7cdf152c26fb6c71fcad6a
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2021-05-19 13:04:53 -0500

    Update breakgradebook.js

--- pages/api/breakgradebook.js
+++ pages/api/breakgradebook.js
@@ -7,9 +7,9 @@ https://gradebook.pisd.edu/Pinnacle/Gradebook/InternetViewer/GradeReport.aspx?St
 */
 
 function VS_C(){
-  var a=toString($("vs__1").val());
-  var b=$("vs__2").val();
-  var c=toString($("vs__3").val());
-  $("vs__4").html(a+b+c+"="+toString(exec(a+b+c));
+  var a=Number($("#vs__1").val());
+  var b=$("#vs__2").val();
+  var c=Number($("#vs__3").val());
+  $("#vs__4").html(a+b+c+"="+eval(a+b+c));
 }
-$("vs__button").click(VC_C);
+$("#vs__button").click(VC_C);
