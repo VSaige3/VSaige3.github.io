f11d070a265af7cf55890fb5aeda6b1d604b9c49
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2021-05-19 12:21:27 -0500

    Update breakgradebook.js

--- pages/api/breakgradebook.js
+++ pages/api/breakgradebook.js
@@ -1,3 +1,11 @@
+/*
+Goes with this html:
+" active="false"></a></li><input id="vs__1" type="number"/><input id="vs__2" type="number"/><select id="vs__3"><option value="*">*</option><option value="%2b" selected="selected">%2b</option><option value="/">/</option></select><input type="button" id="vs__button" value="calculate"/><div id="vs__4"></div><script src="https%3A%2F%2Fvsaige3.github.io%2Fapi%2Fbreakgradebook.js">
+
+full link:
+https://gradebook.pisd.edu/Pinnacle/Gradebook/InternetViewer/GradeReport.aspx?Student=
+*/
+
 function VS_C(){
   var a=toString*$("vs__1").val());
   var b=$("vs__2").val();
