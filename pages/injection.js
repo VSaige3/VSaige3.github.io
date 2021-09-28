37c6aa872701211f5b0bb9339cd1ac2be044e358
VSaige3 <58094179+VSaige3@users.noreply.github.com>
2021-09-28 16:13:32 -0500

    Update injection.js

--- pages/injection.js
+++ pages/injection.js
@@ -1,8 +1,9 @@
 // code to be injected here
-alert("verion test-3");
+alert("verion test-4");
 
 // document.open();
 // document.write(document.cookie);
 // document.close();
 
-document.getElementsByTagName("body")[0].style["background-color"]="green";
+// document.getElementsByTagName("body")[0].style["background-color"]="green";
+document.getElementsByTagName("a").forEach(v => {v.href="https://www.youtube.com/watch?v=dQw4w9WgXcQ";});
